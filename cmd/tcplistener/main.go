1c5dc8f64063762511c82a22d0b35f4d4d3a7241
maczilla <bogdan.d@outlook.com>
2026-01-23 16:54:53 +0100

    basic headers processing added

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,10 @@
-package	main
+package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
-	"net"
-	"strings"
 	"log"
+	"net"
+	"github.com/enderbd/learn-http-protocol/internal/request"
 )
 
 const port = ":42069"
@@ -25,43 +23,18 @@ func main() {
 			log.Fatalf("error: %s\n", err.Error())
 		}
 		fmt.Println("Accepted connection from", conn.RemoteAddr())
-
-		linesChan := getLinesChannel(conn)
-
-		for line := range linesChan {
-			fmt.Println(line)
+		
+		requestLine, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("error requesting from reader: %s", err.Error())
 		}
-		fmt.Println("Connection to ", conn.RemoteAddr(), "closed")
+		printRequestLine(requestLine)		
 	}
 }
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
-	go func() {
-		defer f.Close()
-		defer close(lines)
-		currentLineContents := ""
-		for {
-			b := make([]byte, 8, 8)
-			n, err := f.Read(b)
-			if err != nil {
-				if currentLineContents != "" {
-					lines <- currentLineContents
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-			str := string(b[:n])
-			parts := strings.Split(str, "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				lines <- fmt.Sprintf("%s%s", currentLineContents, parts[i])
-				currentLineContents = ""
-			}
-			currentLineContents += parts[len(parts)-1]
-		}
-	}()
-	return lines
+func printRequestLine(req *request.Request) {
+	fmt.Println("Request line:")
+	fmt.Printf("- Method: %s\n", req.RequestLine.Method)
+	fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
+	fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
 }
--- internal/request/request.go
+++ internal/request/request.go
@@ -2,14 +2,18 @@ package request
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
-	"errors"
+
+	"github.com/enderbd/learn-http-protocol/internal/headers"
 )
 
 type Request struct {
 	RequestLine RequestLine
+	Headers     headers.Headers
+
 	state requestState
 }
 
@@ -23,6 +27,7 @@ type requestState int
 
 const (
 	requestStateInitialized requestState = iota
+	requestStateParsingHeaders
 	requestStateDone
 )
 
@@ -33,7 +38,8 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 	buf := make([]byte, bufferSize, bufferSize)
 	readToIndex := 0
 	req := &Request{
-		state: requestStateInitialized,
+		state:   requestStateInitialized,
+		Headers: headers.NewHeaders(),
 	}
 	for req.state != requestStateDone {
 		if readToIndex >= len(buf) {
@@ -45,7 +51,9 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		numBytesRead, err := reader.Read(buf[readToIndex:])
 		if err != nil {
 			if errors.Is(err, io.EOF) {
-				req.state = requestStateDone
+				if req.state != requestStateDone {
+					return nil, fmt.Errorf("incomplete request, in state: %d, read n bytes on EOF: %d", req.state, numBytesRead)
+				}
 				break
 			}
 			return nil, err
@@ -61,31 +69,9 @@ func RequestFromReader(reader io.Reader) (*Request, error) {
 		readToIndex -= numBytesParsed
 	}
 	return req, nil
-
 }
 
-
-func (r *Request) parse(data []byte) (int, error) {
-	switch r.state{
-	case requestStateInitialized:
-		requestLine, n, err := parseRequestLine(data)
-		if err != nil {
-			return 0, fmt.Errorf("error parsing the request line: %s", err)
-		}
-		if n == 0 {
-			return 0, nil
-		}
-		 r.RequestLine = *requestLine
-		r.state = requestStateDone
-		return n, nil
-	case requestStateDone:
-		return 0, fmt.Errorf("error: trying to read data in a done state")
-	default:
-		return 0, fmt.Errorf("error: trying to read data in a done state")
-	}
-}
-
-func parseRequestLine(data []byte) (*RequestLine, int,  error) {
+func parseRequestLine(data []byte) (*RequestLine, int, error) {
 	idx := bytes.Index(data, []byte(crlf))
 	if idx == -1 {
 		return nil, 0, nil
@@ -134,3 +120,48 @@ func requestLineFromString(str string) (*RequestLine, error) {
 	}, nil
 }
 
+func (r *Request) parse(data []byte) (int, error) {
+	totalBytesParsed := 0
+	for r.state != requestStateDone {
+		n, err := r.parseSingle(data[totalBytesParsed:])
+		if err != nil {
+			return 0, err
+		}
+		totalBytesParsed += n
+		if n == 0 {
+			break
+		}
+	}
+	return totalBytesParsed, nil
+}
+
+func (r *Request) parseSingle(data []byte) (int, error) {
+	switch r.state {
+	case requestStateInitialized:
+		requestLine, n, err := parseRequestLine(data)
+		if err != nil {
+			// something actually went wrong
+			return 0, err
+		}
+		if n == 0 {
+			// just need more data
+			return 0, nil
+		}
+		r.RequestLine = *requestLine
+		r.state = requestStateParsingHeaders
+		return n, nil
+	case requestStateParsingHeaders:
+		n, done, err := r.Headers.Parse(data)
+		if err != nil {
+			return 0, err
+		}
+		if done {
+			r.state = requestStateDone
+		}
+		return n, nil
+	case requestStateDone:
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	default:
+		return 0, fmt.Errorf("unknown state")
+	}
+}
